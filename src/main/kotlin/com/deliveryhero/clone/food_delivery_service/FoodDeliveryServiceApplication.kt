9268a166df935f8fb8c20f78f0c19a435165be29
Fanny Yudris <fanny.yudris@gmail.com>
2025-04-08 17:36:04 +0200

    Initial commit: Spring Boot food-delivery-service

---
+++ src/main/kotlin/com/deliveryhero/clone/food_delivery_service/FoodDeliveryServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.deliveryhero.clone.food_delivery_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodDeliveryServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoodDeliveryServiceApplication>(*args)
+}
