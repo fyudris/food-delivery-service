7f684e3f105f25be71928e66516ea58ef27a4e1c
Fanny Yudris <fanny.yudris@gmail.com>
2025-04-09 03:25:25 +0200

    Adding LoginRequest and LoginResponse dto

---
+++ user-service/src/main/kotlin/com/deliveryhero/clone/user_service/dto/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.deliveryhero.clone.user_service.dto
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
---
+++ user-service/src/main/kotlin/com/deliveryhero/clone/user_service/dto/LoginResponse.kt
@@ -0,0 +1,8 @@
+package com.deliveryhero.clone.user_service.dto
+
+data class LoginResponse(
+    val id: Long,
+    val username: String,
+    val email: String,
+    val message: String
+)
